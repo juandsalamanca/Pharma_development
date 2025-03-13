362e63894987c343573274cfd10973dfa9891b76
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-03-13 17:53:00 -0500

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,7 +3,8 @@ import pandas as pd
 def preprocess(f):
   df = pd.read_excel(f)
   ndc_code_list = df.iloc[3:,0].to_list()
-  return ndc_code_list
+  ndc_count_list = df.iloc[3:,1].to_list()
+  return ndc_code_list, ndc_count_list
 
 def get_manufacturer_and_product_codes(text):
   indexes = [i for i, char in enumerate(text) if char == "-"]
