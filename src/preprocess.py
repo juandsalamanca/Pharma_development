a50955139e4fc79f5cf63d7e0da9677ac057450a
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-04-02 16:56:00 -0500

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -12,3 +12,26 @@ def get_manufacturer_and_product_codes(text):
   product_code = text[indexes[0]+1:indexes[1]]
   package_code = text[indexes[1]+1:]
   return manufacturer_code, product_code, package_code
+
+def fix_ndc_codes(ndc):
+  codes = get_manufacturer_and_product_codes(ndc)
+  name = get_product_name(ndc)
+  time.sleep(2)
+  if name:
+    ndc_code = ndc
+    brand_name, generic_name = name
+    return ndc_code, generic_name, brand_name
+  for i, code in enumerate(codes):
+    if code[0] == "0":
+      new_code = code[1:]
+      if i == 0:
+        ndc_code = new_code +"-"+codes[1]+"-"+codes[2]
+      elif i == 1:
+        ndc_code = codes[0] +"-"+new_code+"-"+codes[2]
+      else:
+        ndc_code = codes[0] +"-" + codes[1] +"-" + new_code
+      name = get_product_name(ndc_code)
+      time.sleep(2)
+      if name:
+        generic_name, brand_name = name
+        return ndc_code, generic_name, brand_name
