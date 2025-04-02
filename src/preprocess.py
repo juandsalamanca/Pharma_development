fa76562f71c7225a037dbf03c2ab5d6c52a13ac0
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-04-02 18:10:22 -0500

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def preprocess(f):
-  colin_df = pd.read_excel("/content/drive/MyDrive/Pharma/skype data 2.0.xlsx", sheet_name="Sheet1")
+  colin_df = pd.read_excel(f, sheet_name="Sheet1")
   ndc_code_list = colin_df["NDC"].to_list()
   return colin_df, ndc_code_list
 
