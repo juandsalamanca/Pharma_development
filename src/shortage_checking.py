313c5218065ad9dbbe7cbad1bc77509a1e426137
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-04-04 12:04:54 -0500

    Update shortage_checking.py

--- src/shortage_checking.py
+++ src/shortage_checking.py
@@ -3,13 +3,14 @@ from src.scraping_tools import get_product_name_from_fda_api
 def check_drug_shortage_fda(ndc, essential_df, fda_ndc_package_code_shortage_list):
   shortage = False
   date = None
+  availbility = None
   if ndc in fda_ndc_package_code_shortage_list:
+    shortage = True
     idx = fda_ndc_package_code_shortage_list.index(ndc)
     date = essential_df.loc[idx, "date_of_update"]
-    if essential_df.loc[idx, "avail_and_esti_short_dur"] == "Unavailable":
-      shortage = True
+    availbility = essential_df.loc[idx, "avail_and_esti_short_dur"]
 
-  return shortage, date
+  return shortage, date, availbility
 
 def check_drug_shortage_ashp(ndcm, df):
 
